37e9e31767e1cffc376a63172cbd2b1d427d0495
benedict-96 <benedikt.brantner@icloud.com>
2024-12-17 15:22:58 +0100

    Not using testset wrapper now.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,5 @@ using GeometricOptimizers
 using SafeTestsets
 using Test
 
-@testset "GeometricOptimizers.jl" begin
-    begin @safetestset "Stiefel Manifold             " include("manifolds/stiefel_manifold.jl") end
-    begin @safetestset "Grassmann Manifold           " include("manifolds/grassmann_manifold.jl") end
-end
+begin @safetestset "Stiefel Manifold             " include("manifolds/stiefel_manifold.jl") end
+begin @safetestset "Grassmann Manifold           " include("manifolds/grassmann_manifold.jl") end
