0c9dda9a43ec9c77dd6ed3828a40117a55563d73
benedict-96 <benedikt.brantner@icloud.com>
2026-04-02 18:18:24 +0200

    Added `OptimzierSolution` and added new methods for `compute_new_iterate!` etc. to generalize to manifolds.

--- src/utils.jl
+++ src/utils.jl
@@ -19,3 +19,30 @@ end
 function add!(dx₁::NamedTuple, dx₂::NamedTuple, dx₃::NamedTuple)
     apply_toNT(add!, dx₁, dx₂, dx₃)
 end
+
+"""
+    OptimizerSolution
+
+A type alias for the solution of an optimizer, which can be either an `AbstractVector` or a [`Manifold`](@ref).
+"""
+const OptimizerSolution{T} = Union{AbstractVector{T},Manifold{T}}
+
+(grad::Gradient{T})(x::Manifold{T}) where {T} = rgrad(x, reshape(grad(vec(x)), size(x)...))
+GradientAutodiff(F, x::StiefelManifold) = GradientAutodiff(_x -> F(StiefelManifold(reshape(_x, size(x)...))), vec(x))
+
+(grad::Gradient{T})(x::Matrix{T}) where {T} = rgrad(x, reshape(grad(vec(x)), size(x)...))
+GradientAutodiff(F, x::Matrix{T}) where {T} = GradientAutodiff(_x -> F(reshape(_x, size(x)...)), vec(x))
+
+function compute_new_iterate!(xₖ₁::Manifold{T}, xₖ::Manifold{T}, α::T, pₖ::AbstractLieAlgHorMatrix{T}, cache::OptimizerCache{T}, retraction_type::AbstractRetraction) where {T}
+    _retraction(x) = retraction(retraction_type, x)
+    update_section!(section(cache), α * pₖ, _retraction)
+    apply_section!(xₖ₁, section(cache), xₖ)
+end
+
+compute_new_iterate!(xₖ::Manifold{T}, α::T, pₖ::AbstractLieAlgHorMatrix{T}, cache::OptimizerCache{T}, retraction_type::AbstractRetraction) where {T} = compute_new_iterate!(xₖ, xₖ, α, pₖ, cache, retraction_type)
+
+function compute_new_iterate!(xₖ::AbstractVector{T}, x::AbstractVector{T}, α::T, pₖ::AbstractVector{T}, cache::OptimizerCache{T}, retraction_type::AbstractRetraction) where {T}
+    _retraction(x) = retraction(retraction_type, x)
+    update_section!(section(cache), α * pₖ, _retraction)
+    apply_section!(xₖ, section(cache), x)
+end
