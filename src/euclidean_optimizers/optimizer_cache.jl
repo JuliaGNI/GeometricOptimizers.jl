b001881baee6dd8c0b9e07a4c82cd5fa1dfafef1
benedict-96 <benedikt.brantner@icloud.com>
2026-04-08 13:32:58 +0200

    Added "TODO" note to docstring.

--- src/euclidean_optimizers/optimizer_cache.jl
+++ src/euclidean_optimizers/optimizer_cache.jl
@@ -2,5 +2,10 @@
     OptimizerCache
 
 See e.g. [`NewtonOptimizerCache`](@ref) and [`BFGSCache`](@ref).
+
+# Extended help
+
+!!! todo
+    `OptimizerCache`s are only used during [`solver_step!`](@ref)s. Outside of these, [`OptimizerState`](@ref)s are used to communicate information between different iterations. This may still have to be enforced consistently.
 """
 abstract type OptimizerCache{T} end
