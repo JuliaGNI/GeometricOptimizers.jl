fad0c8920e67214fe7d51b69278d220bc01ba2c2
benedict-96 <benedikt.brantner@icloud.com>
2026-04-24 13:21:09 +0200

    Using `_copyto!` from `GeometricOptimizers` in the test.
    Using `_copyto!`.

--- test/check_zygote_interfaces.jl
+++ test/check_zygote_interfaces.jl
@@ -1,4 +1,5 @@
 using GeometricOptimizers
+using GeometricOptimizers: _copyto!
 using Zygote
 using Test
 
@@ -8,8 +9,8 @@ const b = (a=a, b=[4.0, 5.0, 6.0])
 loss(a::Vector) = sum(a .^ 2)
 loss(b::NamedTuple{(:a, :b),Tuple{VT,VT}}) where {VT<:Vector} = loss(b.a - b.b)
 
-g₁ = GradientFunction(loss, (b, a) -> (b .= Zygote.gradient(loss, a)[1]), a)
-# g₂ = GradientFunction(loss, (b, a) -> (b .= Zygote.gradient(loss, b))[1], b)
+g₁ = GradientFunction(loss, (_b, _a) -> (_copyto!(_b, Zygote.gradient(loss, _a)[1])), a)
+g₂ = GradientFunction(loss, (_b, _a) -> (_copyto!(_b, Zygote.gradient(loss, _a)[1])), b)
 g₁ᶜ = GradientAutodiff(loss, a)
 
 @test g₁(a) == g₁ᶜ(a)
